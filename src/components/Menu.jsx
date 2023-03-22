fe7a9468ea6e0e4955475f5260068dcfae1e658b
Dima <damkiidmki@yandex.ru>
2023-03-22 21:26:18 +0300

    menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,13 @@
+import {NavLink} from "react-router-dom";
+import React from "react";
+
+export default function Menu() {
+    return (
+        <nav className="menu">
+            <NavLink className={({ isActive }) => isActive ? "menu__item-active": "menu__item"}  to="/" > Главная </NavLink>
+            <NavLink className={({ isActive }) => isActive ? "menu__item-active": "menu__item"}  to="/drift"> Дрифт-такси </NavLink>
+            <NavLink className={({ isActive }) => isActive ? "menu__item-active": "menu__item"}  to="/timeattack" > Time Attack </NavLink>
+            <NavLink className={({ isActive }) => isActive ? "menu__item-active": "menu__item"}  to="/forza"> Forza Karting </NavLink>
+        </nav>
+    );
+}
